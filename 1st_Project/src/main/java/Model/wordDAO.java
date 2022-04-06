3f887c3b9bf23daccf3804ec1331af82af564852
smhrd <smhrd@DESKTOP-SSEAMBB>
2022-04-06 10:32:01 +0900

    wordDTO , wordDAO 생성

---
+++ 1st_Project/src/main/java/Model/wordDAO.java
@@ -0,0 +1,79 @@
+package Model;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class wordDAO {
+
+	Connection conn = null;
+	PreparedStatement psmt = null;
+	ResultSet rs = null;
+	
+	int cnt = 0;
+	userDTO info = null;
+	
+	public void db_conn() {
+		try {
+	
+		Class.forName("oracle.jdbc.driver.OracleDriver");
+		
+		String db_url = "jdbc:oracle:thin:@project-db-stu.ddns.net:1524:xe";
+		String db_id = "campus_k_0325_4";
+		String db_pw = "smhrd4";
+		
+		conn = DriverManager.getConnection(db_url, db_id, db_pw);
+	 	if(conn != null) {
+	 		System.out.println("DB 연결성공");
+		}else {
+			System.out.println("DB 연결실패");
+	 		}
+		}catch (Exception e) {
+				e.printStackTrace();			
+		}
+		
+	}
+
+	public void db_close() {
+		try {
+			if(rs != null) rs.close();
+			if(psmt != null) psmt.close();
+			if(conn != null) conn.close();
+			
+			System.out.println("DB CLOSE");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public ArrayList<wordDTO> wordquiz() {
+		ArrayList<wordDTO> list = new ArrayList<wordDTO>();
+		db_conn();
+		try {
+			
+			String sql = "select * from wordquiz";
+				 
+			psmt = conn.prepareStatement(sql);
+			
+			rs = psmt.executeQuery();
+			
+			
+			while(rs.next()) {
+				String word = rs.getString(1);
+			
+				wordDTO dto = new wordDTO(word);
+				
+				list.add(dto);
+			}
+			
+			}catch (Exception e) {
+				e.printStackTrace();	
+			}finally {
+				db_close();
+			}return list;
+	
+	}
+}
